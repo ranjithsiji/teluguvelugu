d25ed535d634766d3df7c00f246581b210c7967a
Prembabu Panchadara <prem@eggheadcreative.com>
2013-05-14 12:52:02 +0530

    Code Refactored

--- src/com/ehc/teluguvelugu/Dictionary.java
+++ src/com/ehc/teluguvelugu/Dictionary.java
@@ -13,7 +13,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -21,121 +20,114 @@ import android.widget.SearchView;
 import android.widget.TextView;
 
 public class Dictionary extends Activity {
+	public static Date date = new Date();
+	public static String day;
+	public static String word;
+	public static SQLiteDatabase database;
+	public static Typeface typeFace;
 
-  public static Date date = new Date();
-  public static String day;
-  // public static SharedPreferences sharedPreference;
-  public static String word;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button search = (Button) findViewById(R.id.button1);
+		Button random = (Button) findViewById(R.id.button2);
+		final SearchView searchview = (SearchView) findViewById(R.id.searchView1);
+		final TextView result = (TextView) findViewById(R.id.meaning);
+		final Context context = getBaseContext();
+		AssetManager assetmanager = getAssets();
+		typeFace = Typeface.createFromAsset(assetmanager, "Pothana2000.ttf");
+		result.setTypeface(typeFace);
+		DataBaseCopy dbcopy = new DataBaseCopy(context, "dictionary.sqlite", "com.ehc.teluguvelugu");
+		database = dbcopy.openDataBase();
+		result.setText(getWordOfDay());
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    Button search = (Button) findViewById(R.id.button1);
-    Button random = (Button) findViewById(R.id.button2);
-    final SearchView searchview = (SearchView) findViewById(R.id.searchView1);
+		// Giving Functionality to Search Button
+		search.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String word = searchview.getQuery().toString();
+				if (word.equals("")) {
+					result.setText("Please Enter a word.");
+				} else {
+					word = inputConversion(word);
+					String meaning = getMeaning(word);
+					result.setText(meaning);
+				}
+			}
+		});
+		// Giving Functionality to Random Button
+		random.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String randomword = getRandomWord();
+				Intent intent = new Intent(Dictionary.this, RandomActivity.class);
+				intent.putExtra("message", randomword);
+				startActivity(intent);
+			}
+		});
+	}
 
-    final TextView result = (TextView) findViewById(R.id.meaning);
+	@Override
+	protected void onStop() {
+		super.onStop();
+		SharedPreferences sharedPreference = getSharedPreferences("WORDOFDAY", 0);
+		SharedPreferences.Editor editor = sharedPreference.edit();// storing the
+		// value
+		editor.putString(day, word);
+		editor.commit();
+	}
 
-    final Context context = getBaseContext();
-    AssetManager assetmanager = getAssets();
-    Typeface typeFace = Typeface.createFromAsset(assetmanager,
-        "Pothana2000.ttf");
-    result.setTypeface(typeFace);
-    DataBaseCopy dbcopy = new DataBaseCopy(context, "dictionary.sqlite",
-        "com.ehc.teluguvelugu");
-    final SQLiteDatabase database = dbcopy.openDataBase();
+	// Converting given input according to Database Format
+	public String inputConversion(String input) {
+		char array[] = input.toCharArray();
+		array[0] = Character.toUpperCase(input.charAt(0));
+		for (int i = 1; i < array.length; i++) {
+			array[i] = Character.toLowerCase(input.charAt(i));
+		}
+		String changedword = new String(array);
+		Log.d("change word", changedword);
+		return changedword;
+	}
 
-    day = date.getDate() + "";
-    Log.d("dateeeeeeeeeeeeeeeeeeeeeeee", day);
-    SharedPreferences sharedPreference = getSharedPreferences("WORDOFDAY", 0);
+	// Generating Random Word from Database
+	public String getRandomWord() {
+		Cursor result_query = database.rawQuery(
+				"select * from eng2te where rowid = (abs(random()) % (select max(rowid)+1 from eng2te))", null);
+		result_query.moveToFirst();
+		Log.d(result_query.getString(result_query.getColumnIndex("eng_word")),
+				result_query.getString(result_query.getColumnIndex("meaning")));
+		return result_query.getString(result_query.getColumnIndex("eng_word")) + " = "
+				+ result_query.getString(result_query.getColumnIndex("meaning"));
+	}
 
-    String word_day = sharedPreference.getString(day, "");
-    if (word_day.equals("")) {
+	// Getting Meaning for Input Word
+	public String getMeaning(String word) {
+		try {
+			Cursor result_query = database.rawQuery("Select * from eng2te where eng_word='" + word + "'", null);
+			if (result_query.moveToFirst()) {
+				Log.d(word, result_query.getString(result_query.getColumnIndex("meaning")));
+				return result_query.getString(result_query.getColumnIndex("meaning"));
+			} else {
+				return "Sorry, we unable to find word";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-      Cursor cursor = database
-          .rawQuery(
-              "select * from eng2te where rowid = (abs(random()) % (select max(rowid)+1 from eng2te))",
-              null);
-      cursor.moveToFirst();
-
-      word = cursor.getString(cursor.getColumnIndex("eng_word")) + " = "
-          + cursor.getString(cursor.getColumnIndex("meaning"));
-      result.setText(word);
-    } else {
-      result.setText(word_day);
-    }
-
-    search.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        String word = searchview.getQuery().toString();
-        if (word.equals("")) {
-          result.setText("Please Enter a word.");
-        } else {
-          char array[] = word.toCharArray();
-          array[0] = Character.toUpperCase(word.charAt(0));
-          String changedword = new String(array);
-          Log.d("change word", changedword);
-
-          try {
-            Cursor cursor = database.rawQuery(
-                "Select * from eng2te where eng_word='" + changedword + "'",
-                null);
-
-            if (cursor.moveToFirst()) {
-              Log.d(word, cursor.getString(cursor.getColumnIndex("meaning")));
-              result.setText(cursor.getString(cursor.getColumnIndex("meaning")));
-            } else {
-              result.setText("Sorry, we unable to find word");
-            }
-          } catch (SQLException e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    });
-
-    random.setOnClickListener(new OnClickListener() {
-
-      @Override
-      public void onClick(View v) {
-
-        Cursor cursor = database
-            .rawQuery(
-                "select * from eng2te where rowid = (abs(random()) % (select max(rowid)+1 from eng2te))",
-                null);
-
-        cursor.moveToFirst();
-        Log.d(cursor.getString(cursor.getColumnIndex("eng_word")),
-            cursor.getString(cursor.getColumnIndex("meaning")));
-
-        Intent intent = new Intent(Dictionary.this, RandomActivity.class);
-        intent.putExtra("message",
-            cursor.getString(cursor.getColumnIndex("eng_word")) + " = "
-                + cursor.getString(cursor.getColumnIndex("meaning")));
-
-        startActivity(intent);
-
-      }
-    });
-  }
-
-  @Override
-  protected void onStop() {
-    // TODO Auto-generated method stub
-    super.onStop();
-    SharedPreferences sharedPreference = getSharedPreferences("WORDOFDAY", 0);
-    SharedPreferences.Editor editor = sharedPreference.edit();// storing the
-    // value
-    editor.putString(day, word);
-    editor.commit();
-
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
-  }
+	// Getting Word Of The Day
+	public String getWordOfDay() {
+		day = date.getDate() + "";
+		Log.d("dateeeeeeeeeeeeeeeeeeeeeeee", day);
+		SharedPreferences sharedPreference = getSharedPreferences("WORDOFDAY", 0);
+		String word_day = sharedPreference.getString(day, "");
+		if (word_day.equals("")) {
+			word = getRandomWord();
+			return word;
+		} else {
+			return word_day;
+		}
+	}
 }
