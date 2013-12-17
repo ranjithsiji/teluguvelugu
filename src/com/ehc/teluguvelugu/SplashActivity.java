ae0fff9765412a99c74ca0342ca54890260def4a
prembabu <prem@eggheadcreative.com>
2013-12-17 09:53:16 +0530

    Redused Splash Delay

--- src/com/ehc/teluguvelugu/SplashActivity.java
+++ src/com/ehc/teluguvelugu/SplashActivity.java
@@ -7,19 +7,19 @@ import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-		int delay = 3000;
+        int delay = 2000;
 
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				startActivity(new Intent(SplashActivity.this, DictionaryActivity.class));
-				finish();
-			}
-		}, delay);
-	}
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, DictionaryActivity.class));
+                finish();
+            }
+        }, delay);
+    }
 }
